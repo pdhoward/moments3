81657c4001d539a05c4e1ca2fa99f195bc7bae55
pdhoward <patrick.howard@hotmail.com>
2019-04-17 12:12:46 -0400

    initialize

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,32 +1,32 @@
 const colors = require("../../src/styles/colors");
 
 module.exports = {
-  siteTitle: "PersonalBlog - a blog starter for GatsbyJS", // <title>
-  shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
-  siteDescription: "PersonalBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  siteTitle: "Moments - Member Engagement Platform", // <title>
+  shortSiteTitle: "Moments", // <title> ending for posts and pages
+  siteDescription: "PMember Engagement and Dialogue Platform",
+  siteUrl: "",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Strategic Machines",
+  authorTwitterAccount: "urban_coder",
   // info
-  infoTitle: "greg lobinski",
-  infoTitleNote: "personal blog",
+  infoTitle: "Strategic Machines",
+  infoTitleNote: "member engagement",
   // manifest.json
-  manifestName: "PersonalBlog - a blog starter for GatsbyJS",
-  manifestShortName: "PersonalBlog", // max 12 characters
+  manifestName: "Moments - a member engagement platform",
+  manifestShortName: "Moments", // max 12 characters
   manifestStartUrl: "/",
   manifestBackgroundColor: colors.background,
   manifestThemeColor: colors.background,
   manifestDisplay: "standalone",
   // contact
-  contactEmail: "john@doe.com",
+  contactEmail: "patrick.howard@xiollc.com",
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "https://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/pdhoward" },
+    { name: "twitter", url: "https://twitter.com/urban_coder" },
+    { name: "facebook", url: "" }
   ]
 };
